82aa16fcd886ab2180818620811e91f1a9203366
Eziane <ezianabderrahim@gmail.com>
2024-06-02 21:31:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import Calculator from './calculator';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Calculator</h1>
+        <Calculator />
+        <footer className="App-footer">
+          <p>
+            Made with ❤️ by Eziane Abderrahim.
+            <a href="https://www.linkedin.com/in/abderrahim-eziane-354b15210/" target="_blank" rel="noopener noreferrer">
+              Checkout my LinkedIn, let's get to know each other.
+            </a>
+          </p>
+        </footer>
       </header>
     </div>
   );
